c18b9ec985f57ce201f92d8692d1a1196910f350
deepika845 <its.deepika08@gmail.com>
2020-07-25 15:02:19 +0530

    Max Product in matrix from top left to bottom right using DP

--- src/com/deepika/problem/solving/Interviewquestions/FindMaxProduct.java
+++ src/com/deepika/problem/solving/Interviewquestions/FindMaxProduct.java
@@ -30,7 +30,7 @@ public class FindMaxProduct {
             }
         }
         return maxArray[arr.length-1][arr.length-1];
-    }
+    }f
     public static void main(String[] args) {
         int arr[][] = {{-1,2,3},{4,5,-6},{7,8,9}};
         System.out.println(findMaxProduct(arr));
