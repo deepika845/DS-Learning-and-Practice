1531516bac231c2f393a8ec24d9ecf6e132338ef
deepika845 <its.deepika08@gmail.com>
2020-08-01 15:54:02 +0530

    Add client class of word square problem

--- src/com/deepika/problem/solving/Interviewquestions/WordSquareResqueClient.java
+++ src/com/deepika/problem/solving/Interviewquestions/WordSquareResqueClient.java
@@ -1,4 +1,10 @@
 package com.deepika.problem.solving.Interviewquestions;
 
-public class WordSquareResqueClient {
+public class WordSquareResqueClient
+{
+    public static void main(String[] args) {
+    WordSquareResque ww = new WordSquareResque();
+        System.out.println(ww.wordSquares(new String[]{"ember","heart","resin","trend","abuse"}));
+
+    }
 }
