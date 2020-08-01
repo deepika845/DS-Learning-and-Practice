da12774b9b7a2e43ffe8800cf44af034ec916cf2
deepika845 <its.deepika08@gmail.com>
2020-08-01 18:19:43 +0530

    Add implementation of Queue using two Stacks

--- src/com/deepika/problem/solving/Interviewquestions/ImplementQueueUsingTwoStack.java
+++ src/com/deepika/problem/solving/Interviewquestions/ImplementQueueUsingTwoStack.java
@@ -1,16 +1,30 @@
 package com.deepika.problem.solving.Interviewquestions;
-
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
 public class ImplementQueueUsingTwoStack {
-   Queue<Integer> q1 = new LinkedList<Integer>();
-   Queue<Integer> q2 = new LinkedList<Integer>();
+  Stack<Integer> s1 = new Stack<>();
+  Stack<Integer> s2 = new Stack<>();
    public void enqueue(int ele){
-
+       s1.push(ele);
+   }
+   public int dequeue(){
+       if(s1.isEmpty()){
+           return -1;
+       }
+       while (!s1.isEmpty()){
+           int p = s1.pop();
+           s2.push(p);
+       }
+       return s2.pop();
    }
     public static void main(String[] args) {
+       ImplementQueueUsingTwoStack nn = new ImplementQueueUsingTwoStack();
+       nn.enqueue(4);
+       nn.enqueue(3);
+        nn.enqueue(2);
+        nn.enqueue(1);
+        nn.enqueue(0);
+        System.out.println(nn.dequeue());
 
     }
 }
