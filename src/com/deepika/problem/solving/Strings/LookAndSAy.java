0d7c532556745b3ad5a6cfba9a82a29edc83a064
deepika845 <its.deepika08@gmail.com>
2020-08-29 19:35:29 +0530

    Add implementation of Look And Say Sequence

---
+++ src/com/deepika/problem/solving/Strings/LookAndSAy.java
@@ -0,0 +1,37 @@
+package com.deepika.problem.solving.Strings;
+
+public class LookAndSAy {
+    public static void main(String[] args) {
+        System.out.println(findNthLookAndSay(4).toString());
+       // System.out.println(s);
+    }
+
+    private static StringBuilder findNthLookAndSay(int i) {
+        return findNthLookAndSay1(i,1,new StringBuilder("1"));
+    }
+    private static StringBuilder findNthLookAndSay1(int i,int j,StringBuilder s){
+        if(i==j){
+            return s;
+        }
+        int index=1;
+        StringBuilder sb =new StringBuilder();
+        for (int k=0;k<s.length();k++){
+
+            if(k==s.length()-1){
+                sb.append(index);
+                sb.append(s.charAt(k));
+                break;
+            }
+            if(s.charAt(k)==s.charAt(k+1)){
+                index++;
+                continue;
+            }
+            else {
+                sb.append(index);
+                sb.append(s.charAt(k));
+            }
+        }
+        return findNthLookAndSay1(i,j+1,sb);
+    }
+
+}
