2befdbd3786459017c0df2eefc40b4222654a327
deepika845 <its.deepika08@gmail.com>
2020-09-01 20:36:34 +0530

    Add implementation of finding sinusoidal pattern in a string

--- src/com/deepika/problem/solving/Strings/SinusoidalPattern.java
+++ src/com/deepika/problem/solving/Strings/SinusoidalPattern.java
@@ -1,4 +1,33 @@
 package com.deepika.problem.solving.Strings;
 
 public class SinusoidalPattern {
+    public static void main(String[] args) {
+        sinusoidal("helloworld");
+    }
+
+    private static void sinusoidal(String s) {
+        StringBuilder sb =new StringBuilder();
+        sb.append(" ");
+        for(int i=1;i<s.length() ;i+=4 ){
+           sb.append(s.charAt(i));
+            sb.append("     ");
+        }
+        sb.append("\n");
+        System.out.print(sb);
+        sb=new StringBuilder("");
+        for (int i=0;i<s.length();i+=2){
+            sb.append(s.charAt(i));
+            sb.append("  ");
+        }
+        sb.append("\n    ");
+        System.out.print(sb);
+        sb=new StringBuilder("");
+        for (int i=3;i<s.length();i+=4){
+            sb.append(s.charAt(i));
+            sb.append("     ");
+        }
+        sb.append("\n  ");
+        System.out.print(sb);
+    }
+
 }
