2801ce66a3720144f2c1b6fe8f9a2a1fd0255861
deepika845 <its.deepika08@gmail.com>
2020-09-20 16:59:50 +0530

    Add implementation of finding all paths with specified sum in a tree

--- src/com/deepika/problem/solving/TreesLecture/LcaPredictor.java
+++ src/com/deepika/problem/solving/TreesLecture/LcaPredictor.java
@@ -1,11 +1,14 @@
 package com.deepika.problem.solving.TreesLecture;
 
-//import org.graalvm.compiler.phases.common.inlining.InliningPhase_OptionDescriptors;
 
-import javax.crypto.spec.PSource;
+
+//import javax.crypto.spec.PSource;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class LcaPredictor {
+    static Vector<Integer> path = new Vector<Integer>();
     Node root;
     class Node{
         int ch;
@@ -89,7 +92,8 @@ public class LcaPredictor {
         //System.out.println(ll.root.ch);
      // System.out.println( ll.findLCA('A','E'));
       //  System.out.println( ll.findRootToLeaf());
-        System.out.println(ll.checkPathWithSpecifiedSum(11));
+        //System.out.println(ll.checkPathWithSpecifiedSum(11));
+        ll.findPath(11);
     }
     public int findRootToLeaf(){
         return findRootToLeaf(root,0);
@@ -119,11 +123,29 @@ public class LcaPredictor {
         }
         return checkPathWithSpecifiedSum(node.left,sum-node.ch) || checkPathWithSpecifiedSum(node.right,sum-node.ch);
     }
+    public void findPath(int sum){
+        findPath(root,sum);
+    }
+    public void findPath(Node root,int sum){
+        if(root==null){
+            return;
+        }
+        path.add(root.ch);
+        findPath(root.left,sum);
+        findPath(root.right,sum);
+        int ff=0;
+        int len =path.size();
+        for(int i=0;i<path.size();i++){
+            ff+=path.get(i);
+        }
+        if(ff==sum){
+            System.out.println(path);
+        }
+        path.remove(path.size()-1);
+
+    }
 
 }
 //       yes A yes C n n yes F yes D n n yes E n n
-
-
-
-
-       //yes 1 yes 0 yes 1 n m yes 0 no no yes 0 yes 1 0 0 yes 0 n n
+//yes 1 yes 0 yes 1 n m yes 0 no no yes 0 yes 1 0 0 yes 0 n n
+//yes 1 yes 3 yes 7 n nn yes 8 n n yes 4 yes 5 n yes 1 n n yes 6 n n
