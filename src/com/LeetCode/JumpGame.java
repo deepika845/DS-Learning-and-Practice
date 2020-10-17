112af1576669c0c99fe51f7898b5ce60f6537845
deepika845 <its.deepika08@gmail.com>
2020-10-17 18:23:30 +0530

    Add implementation of jump game using array

---
+++ src/com/LeetCode/JumpGame.java
@@ -0,0 +1,20 @@
+package com.LeetCode;
+
+public class JumpGame {
+    public static int findMinJumps(int arr[]){
+        int jumps=0;
+        int currarthest=0;
+        int currEnd=0;
+        for(int i=0;i<arr.length;i++){
+            currarthest=Math.max(currarthest,i+arr[i]);
+            if(i==currEnd){
+                jumps++;
+                currEnd=currarthest;
+            }
+        }
+        return jumps;
+    }
+    public static void main(String[] args) {
+     findMinJumps(new int[]{2,4,1,1,8});
+    }
+}
