ddb1ef5d4536876abe950620bec588144db57fb0
deepika845 <its.deepika08@gmail.com>
2020-10-17 18:24:47 +0530

    Finding subset of an array using backtrcking

--- src/com/LeetCode/Subsets.java
+++ src/com/LeetCode/Subsets.java
@@ -1,4 +1,27 @@
 package com.LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Subsets {
+    static public List<List<Integer>>  subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+   static private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        subsets(new int[]{3,2,5,1,8,6});
+    }
 }
