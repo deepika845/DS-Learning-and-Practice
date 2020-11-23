8bb366203b7cce1e6d079b2957c179af39f3fc78
deepika845 <its.deepika08@gmail.com>
2020-11-23 21:07:42 +0530

    Add implementation of printing sinusoidal pattern of a string

--- src/com/deepika/problem/solving/Strings/SinusoidalPrint.java
+++ src/com/deepika/problem/solving/Strings/SinusoidalPrint.java
@@ -1,4 +1,35 @@
 package com.deepika.problem.solving.Strings;
 
+import java.util.Arrays;
+
 public class SinusoidalPrint {
+    public static char[][] findSinuSoidalPattern(String s,int k){
+        boolean down = true;
+        int col =0;
+        char[][] pattern = new char[k][s.length()];
+        int row=0;
+        for (int i=0;i<s.length();i++){
+            char ff = s.charAt(i);
+            pattern[row][i]=ff;
+            if(row==k-1){
+                down=false;
+            }
+            if (row==0){
+                down=true;
+            }
+            if (down) {
+                row++;
+            } else {
+                row--;
+            }
+        }
+        return pattern;
+    }
+
+    public static void main(String[] args) {
+      char[][] pp=  findSinuSoidalPattern("abcdef",3);
+        for (int i=0;i<pp.length;i++){
+            System.out.println(Arrays.toString(pp[i]));
+        }
+    }
 }
