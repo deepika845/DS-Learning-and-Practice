a7be3374d8f82e2fff11c8a31bb11ad5ea208402
deepika845 <its.deepika08@gmail.com>
2021-02-12 23:28:06 +0530

    Implementation of inorder traversal using stack

---
+++ src/com/deepika/problem/solving/Stacks/InorderStack.java
@@ -0,0 +1,41 @@
+package com.deepika.problem.solving.Stacks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class InorderStack {
+   static   class TreeNode {
+      int val;
+     TreeNode left;
+      TreeNode right;
+
+      TreeNode(int val) { this.val = val; }
+  }
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stackMaintained= new Stack<TreeNode>();
+        List<Integer> inorder = new ArrayList<Integer>();
+        TreeNode curr= root;
+        while(curr!=null || !stackMaintained.isEmpty()){
+            //stackMaintained.push(curr);
+            while(curr!=null){
+                stackMaintained.push(curr);
+                curr=curr.left;
+            }
+            TreeNode poped=stackMaintained.pop();
+            inorder.add(poped.val);
+            curr=poped.right;
+        }
+        return inorder;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right=new TreeNode(2);
+        root.right.left=new TreeNode(3);
+        System.out.println(inorderTraversal(root));
+
+
+
+    }
+}
