06fd03abf4b036f18d2a00328a45abc03d63748d
deepika845 <its.deepika08@gmail.com>
2021-03-24 10:41:02 +0530

    Add implementation of search and sort in sorted rotated array

--- src/com/deepika/problem/solving/array/SearchSortedRotated.java
+++ src/com/deepika/problem/solving/array/SearchSortedRotated.java
@@ -1,4 +1,34 @@
 package com.deepika.problem.solving.array;
 
 public class SearchSortedRotated {
+    public static int findPosition(int[] arr,int item,int start,int end){
+        while (start<=end){
+            int mid=(start+end)/2;
+            if (arr[mid]==item){
+                return mid;
+            }
+            if (arr[mid]>=arr[start]){
+                if (item>=arr[start] && item<arr[mid] ){
+                    end=mid-1;
+                }
+                else {
+                    start=mid+1;
+                }
+            }
+            else {
+                if (item<=arr[end] && item>arr[mid] ){
+                    start=mid+1;
+                }
+                else {
+                    end=mid-1;
+                }
+
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(findPosition(new int[]{5,6,7,0,1,2,3,4},0,0,7));
+
+    }
 }
